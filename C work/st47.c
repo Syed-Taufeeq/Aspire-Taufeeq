c333eb4eee84bfa142bc74f5be1bc71cd7e3cbe8
Syed-Taufeeq <98599513+Syed-Taufeeq@users.noreply.github.com>
2022-10-25 20:00:52 +0530

    Add files via upload

---
+++ C work/st47.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+double num;
+printf("Enter a number: ");
+scanf("%lf", &num);
+if (num <= 0.0) {
+if (num == 0.0)
+printf("You entered 0.");
+Compiled and Presented by – M.H. Shoiab.
+else
+printf("You entered a negative number.");
+}
+else
+printf("You entered a positive number.");
+return 0;
+}
